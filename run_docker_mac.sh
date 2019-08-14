aee5598b8ee2d23131fc63b2d687209c52514adf
athro <a@karwath.org>
2019-08-14 14:49:02 +0100

    added run scripts

---
+++ run_docker_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}' )
+xhost +
+
+IMAGE='altermeister/hdruk_summerschool_session_1_2'
+NAME='hdruk_s12'
+FOLDER='/Users/karwath/bearshare.bham/teaching/2019/summerSchool/docker/chiara/HDRUK_omics/data'
+
+# This command will start the container on background mode
+CMD="docker run \
+    --rm -d \
+    --privileged \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -e DISPLAY=$ip:0 \
+    --name $NAME \
+    -v $FOLDER/:/Data/:rw \
+    -ti $IMAGE
+"
+
+echo "CMD: [$CMD]" >&2
+ret= eval $CMD
+exit $ret
+
