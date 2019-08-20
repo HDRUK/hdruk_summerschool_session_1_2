cf38c5b74a9e5b8a8397bac097478b4245b00fe6
athro <a@karwath.org>
2019-08-20 10:20:53 +0100

    update run docker commands for mac & linux session2

--- run_docker_mac.sh
+++ run_docker_mac.sh
@@ -3,21 +3,31 @@ ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}' )
 xhost +
 
 IMAGE='altermeister/hdruk_summerschool_session_1_2'
-NAME='hdruk_s12'
-FOLDER='/Users/<user_name>/data'
+NAME='hdruk_s2'
+FOLDER='/Users/<<USER>>/data' # PLEASE CHANGE
 
 # This command will start the container on background mode
 CMD="docker run \
-    --rm -d \
-    --privileged \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -e DISPLAY=$ip:0 \
-    --name $NAME \
-    -v $FOLDER/:/Data/:rw \
-    -ti $IMAGE
+--rm -d \
+--privileged \
+-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+-e DISPLAY=$ip:0 \
+--name $NAME \
+-v $FOLDER/:/Data/:rw \
+-ti $IMAGE
 "
+# try and stop in case it is already running
+CMD_STOP="docker stop ${NAME}"
+printf "CMD_STOP: [$CMD_STOP]\n" >&2
+ret_stop= eval $CMD_STOP
+printf "\n\n"
 
-echo "CMD: [$CMD]" >&2
+echo "CMD: $CMD" >&2
 ret= eval $CMD
+printf "\n\n"
+
+echo "Now start the image using the following command:"
+printf "\n"
+echo "docker exec -it ${NAME} /bin/bash"
 exit $ret
 
